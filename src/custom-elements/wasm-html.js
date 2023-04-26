e8095fc7eac38e97356fe464f628bf5b4f18a85c
Patrick Smith <pgwsmith@gmail.com>
2023-04-26 21:26:03 +1000

    Add form for svg rect color

--- src/custom-elements/wasm-html.js
+++ src/custom-elements/wasm-html.js
@@ -1,25 +1,52 @@
 class WasmHTML extends HTMLElement {
-    connectedCallback() {
+    static get observedAttributes() { return ['globals']; }
+
+    async attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "globals") {
+            const { exports, update } = await this.main;
+            const newValues = new Map(Object.entries(JSON.parse(newValue)));
+
+            for (const [key, stringValue] of newValues) {
+                if (typeof stringValue === "string") {
+                    const int = parseInt(stringValue);
+                    if (!Number.isNaN(int)) {
+                        exports[key].value = int;
+                    }
+                }
+            }
+
+            update();
+        }
+    }
+
+    constructor() {
+        super();
+
+        console.log("connectedCallback");
         const wasmURL = this.dataset.url;
         if (wasmURL) {
-            const memory = new WebAssembly.Memory({ initial: 2 });
-            const wasmInstancePromise = WebAssembly.instantiateStreaming(fetch(wasmURL, { credentials: "omit" }), {
-                env: {
-                    buffer: memory
+            const wasmModulePromise = WebAssembly.compileStreaming(fetch(wasmURL, { credentials: "omit" }));
+
+            const makeInstancePromise = wasmModulePromise.then(module => {
+                console.log("MODULE", module);
+                return async function makeInstance() {
+                    const memory = new WebAssembly.Memory({ initial: 2 });
+                    const instance = await WebAssembly.instantiate(module, {
+                        env: {
+                            buffer: memory
+                        }
+                    });
+                    return ({ instance, memory });
                 }
-            })
-                .then(a => {
-                    console.log("Loaded wasm directly", Date.now() - window.startTime);
-                    return a;
-                });
-            initWasmHTML(this, wasmInstancePromise, memory);
+            });
+
+            this.main = initWasmHTML(this, makeInstancePromise);
         }
 
         if (this.dataset.scriptUrl) {
-            const memory = new WebAssembly.Memory({ initial: 2 });
             const wasmModulePromise = window.importModule(this.dataset.scriptUrl);
             console.log("import(this.dataset.scriptUrl)", wasmModulePromise);
-            const wasmInstancePromise = wasmModulePromise
+            const makeInstancePromise = wasmModulePromise
                 .then(exports => {
                     console.log("Loaded wasm via import()", Date.now() - window.startTime);
                     console.log("ES MODULE", exports);
@@ -27,67 +54,70 @@ class WasmHTML extends HTMLElement {
                 })
                 .then(module => {
                     console.log("MODULE", module);
-                    const instancePromise = WebAssembly.instantiate(module, {
-                        env: {
-                            buffer: memory
-                        }
-                    });
-                    return instancePromise;
-                })
-                .then(instance => ({ instance }));
-            initWasmHTML(this, wasmInstancePromise, memory);
+                    return async function makeInstance() {
+                        const memory = new WebAssembly.Memory({ initial: 2 });
+                        const instance = await WebAssembly.instantiate(module, {
+                            env: {
+                                buffer: memory
+                            }
+                        });
+                        return ({ instance, memory });
+                    }
+                });
+
+            this.main = initWasmHTML(this, makeInstancePromise);
         }
     }
 }
 
-function initWasmHTML(el, wasmInstancePromise, memory) {
-    wasmInstancePromise.then(({ instance }) => {
+async function initWasmHTML(el, makeInstancePromise) {
+    const makeInstance = await makeInstancePromise;
+    let { instance, memory } = await makeInstance();
+    console.log("im", instance, memory)
+
+    const memoryBytes = new Uint8Array(memory.buffer);
+    const utf8decoder = new TextDecoder();
+
+    function update() {
         const rewind = instance.exports.rewind;
         const next_body_chunk = instance.exports.next_body_chunk;
+        rewind();
 
-        const memoryBytes = new Uint8Array(memory.buffer);
-        //const memoryToWrite = memoryBytes.subarray(1024);
-        //const memoryToRead = memoryBytes.subarray(2048);
-        const utf8encoder = new TextEncoder();
-        const utf8decoder = new TextDecoder();
-
-        function update() {
-            rewind();
-
-            const chunks = [];
-            //const chunks = new Uint8Array(1000);
-            while (true) {
-                const ptr = next_body_chunk();
-                if (ptr === 0) {
-                    break;
-                }
-
-                // Search for null-terminating byte.
-                const endPtr = memoryBytes.indexOf(0, ptr);
-                // Get subsection of memory between start and end, and decode it as UTF-8.
-                //return utf8decoder.decode(memoryBytes.subarray(ptr, endPtr));
-                //chunks.concat(memoryToRead.subarray(0, count));
-                chunks.push(memoryBytes.subarray(ptr, endPtr));
-                //chunks.set(memoryBytes.subarray(ptr, endPtr), chunks.length);
+        const chunks = [];
+        //const chunks = new Uint8Array(1000);
+        while (true) {
+            const ptr = next_body_chunk();
+            if (ptr === 0) {
+                break;
             }
 
-            // There surely must be a better way to do this.
-            // See: https://stackoverflow.com/questions/49129643/how-do-i-merge-an-array-of-uint8arrays
-            const bytes = new Uint8Array(chunks.map(chunk => [...chunk]).flat());
-            const html = utf8decoder.decode(bytes);
-            el.innerHTML = html;
+            // Search for null-terminating byte.
+            const endPtr = memoryBytes.indexOf(0, ptr);
+            // Get subsection of memory between start and end, and decode it as UTF-8.
+            //return utf8decoder.decode(memoryBytes.subarray(ptr, endPtr));
+            //chunks.concat(memoryToRead.subarray(0, count));
+            chunks.push(memoryBytes.subarray(ptr, endPtr));
+            //chunks.set(memoryBytes.subarray(ptr, endPtr), chunks.length);
         }
 
-        el.addEventListener("click", (event) => {
-            const action = event.target.dataset.action;
-            if (typeof action === "string") {
-                instance.exports[action]?.apply();
-            }
-            update();
-        });
+        // There surely must be a better way to do this.
+        // See: https://stackoverflow.com/questions/49129643/how-do-i-merge-an-array-of-uint8arrays
+        const bytes = new Uint8Array(chunks.map(chunk => [...chunk]).flat());
+        const html = utf8decoder.decode(bytes);
+        el.innerHTML = html;
+    }
 
-        queueMicrotask(update);
+    el.addEventListener("click", (event) => {
+        const action = event.target.dataset.action;
+        if (typeof action === "string") {
+            instance.exports[action]?.apply();
+        }
+        update();
     });
+
+    queueMicrotask(update);
+
+    return { exports: instance.exports, update };
 }
 
 customElements.define("wasm-html", WasmHTML);
