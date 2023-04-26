f9cf448c3bbdcf8526a7de5415fdbaf735411855
Patrick Smith <pgwsmith@gmail.com>
2023-04-26 19:47:04 +1000

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import deno from '@astrojs/deno';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: deno(),
-    server: {
-        port: 5888,
-    },
-    integrations: [
-        mdx()
-    ],
+  output: 'server',
+  adapter: deno(),
+  server: {
+    port: 5888
+  },
+  integrations: [mdx(), tailwind()]
 });
