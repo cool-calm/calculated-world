1d069aea63d33aab9d8eefc7e8b5a2b386b3b863
Patrick Smith <pgwsmith@gmail.com>
2023-04-26 14:25:41 +1000

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: deno(),
+    server: {
+        port: 5888,
+    },
+    integrations: [
+        mdx()
+    ],
+});
