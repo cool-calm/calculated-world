f478f1ad30227cd0d2a19e2221ec91f4a0d95301
Patrick Smith <pgwsmith@gmail.com>
2024-01-26 12:21:31 +1100

    Disable fix-shiki-deno.ts

--- src/fix-shiki-deno.ts
+++ src/fix-shiki-deno.ts
@@ -1,8 +1,8 @@
-import * as shiki from "shiki";
+// import * as shiki from "shiki";
 
-export async function fixShikiDeno() {
-    const responseWasm = await fetch("https://unpkg.com/shiki/dist/onig.wasm");
-    const wasmArrayBuffer = await responseWasm.arrayBuffer();
-    shiki.setWasm(wasmArrayBuffer);
-    shiki.setCDN("https://unpkg.com/shiki/");
-}
+// export async function fixShikiDeno() {
+//     const responseWasm = await fetch("https://unpkg.com/shiki/dist/onig.wasm");
+//     const wasmArrayBuffer = await responseWasm.arrayBuffer();
+//     shiki.setWasm(wasmArrayBuffer);
+//     shiki.setCDN("https://unpkg.com/shiki/");
+// }
